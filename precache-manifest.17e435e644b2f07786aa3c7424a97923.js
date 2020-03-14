aedcaf915487e9bfd8a2e96a79af0c92496bd128
KinKam Zhao <kamzh88@hotmail.com>
2020-03-13 22:12:34 -0400

    Updates

---
+++ precache-manifest.17e435e644b2f07786aa3c7424a97923.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c449d3496ff0409df54fbbe1f79910c2",
+    "url": "/monster-rolodex.github.io/index.html"
+  },
+  {
+    "revision": "432d808a6d4d51bf59dd",
+    "url": "/monster-rolodex.github.io/static/css/main.364f90ad.chunk.css"
+  },
+  {
+    "revision": "99c3a51d2ab1eacd0868",
+    "url": "/monster-rolodex.github.io/static/js/2.43e0b3ed.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-rolodex.github.io/static/js/2.43e0b3ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "432d808a6d4d51bf59dd",
+    "url": "/monster-rolodex.github.io/static/js/main.d03ba171.chunk.js"
+  },
+  {
+    "revision": "1725af654f333671641f",
+    "url": "/monster-rolodex.github.io/static/js/runtime-main.d8ba9643.js"
+  }
+]);
