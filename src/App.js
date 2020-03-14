82a40256537c3d32da4021464618932a82a049cb
KinKam Zhao <kamzh88@hotmail.com>
2020-03-13 22:13:37 -0400

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { CardList } from "./components/card-list/card-list.component.jsx";
+import SearchBox from "./components/search-box/search-box.component";
+import './App.css'
 
 class App extends Component {
   constructor() {
@@ -16,6 +18,10 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  }
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster =>
@@ -23,10 +29,10 @@ class App extends Component {
     );
     return (
       <div className="App">
-        <input
-          type="search"
-          placeholder="search monsters"
-          onChange={e => this.setState({ searchField: e.target.value })}
+      <h1 className="title">Monster Rolodex</h1>
+        <SearchBox
+          placeholder={"search monster"}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters}></CardList>
       </div>
